2915b318aaf59697b6f0108409bc86da4b5d55f8
Nikita <nikturchinovich@mail.ru>
2023-10-26 11:59:58 +0300

    lab06 is done

---
+++ lab06/lab06/Exceptions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class ProductException : Exception {
+    public ProductException() : base() { }
+
+    public ProductException(string message) : base(message) { }
+}
+
+
+public class ProductPriceException : ProductException {
+    public decimal? InvalidPrice = null;
+
+    public ProductPriceException() : base("Invalid product price") { }
+
+    public ProductPriceException(string message) : base(message) { }
+
+    public ProductPriceException(decimal price) : this($"Invalid product price: {price}") {
+        InvalidPrice = price;
+    }
+
+    public ProductPriceException(string message, decimal price) : this(message) {
+        InvalidPrice = price;
+    }
+}
+
+
+public class ProductWeightException : ProductException {
+    public decimal? InvalidWeight = null;
+
+    public ProductWeightException() : base("Invalid product weight") { }
+
+    public ProductWeightException(string message) : base(message) { }
+
+    public ProductWeightException(decimal weight) : this($"Invalid product weight: {weight}") {
+        InvalidWeight = weight;
+    }
+
+    public ProductWeightException(string message, decimal weight) : this(message) {
+        InvalidWeight = weight;
+    }
+}
---
+++ lab06/lab06/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Main started");
+        try {
+            Sofa sofa = new Sofa();
+            Bed bed = new Bed();
+            Wardrobe wardrobe = new Wardrobe();
+
+            sofa.Weight = 15;
+            bed.Price = 235;
+            wardrobe.Price = 205;
+            wardrobe.Weight = 79;
+
+            try {
+                sofa.Price = -178;
+            }
+            catch (ProductPriceException ex) {
+                Console.WriteLine($"{ex.GetType().Name}: '{ex.Message}' ({ex.InvalidPrice})");
+                sofa.Price = 178;
+            }
+
+            try {
+                bed.Weight = 0;
+            }
+            catch (ProductWeightException ex) {
+                Console.WriteLine($"{ex.GetType().Name}: '{ex.Message}' ({ex.InvalidWeight})");
+                bed.Weight = 47;
+            }
+
+            try {
+                wardrobe.Manufacturer = null;
+            }
+            catch (ProductException ex) {
+                Console.WriteLine($"{ex.GetType().Name}: '{ex.Message}'");
+            }
+
+            try {
+                Debug.Assert(wardrobe.Manufacturer != null, "Wardrobe manufacturer is null!");
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex);
+            }
+
+            Warehouse warehouse = new Warehouse();
+            warehouse.AddProduct(sofa);
+            warehouse.AddProduct(bed);
+            warehouse.AddProduct(wardrobe);
+
+            try {
+                Console.WriteLine("[START] Try to get 'warehouse[4]'");
+                Console.WriteLine(warehouse[4]);
+            }
+            catch (IndexOutOfRangeException ex) {
+                Console.WriteLine($"{ex.GetType().Name}: {ex.Message}");
+            }
+            catch (Exception ex){
+                Console.WriteLine("Some unexpexted exception");
+                throw ex;
+            }
+            finally {
+                Console.WriteLine("[END] Try to get 'warehouse[4]'");
+            }
+        }
+        catch (Exception ex) {
+            Console.WriteLine("Unexpected Exception");
+            Console.WriteLine(ex);
+        }
+        finally {
+            Console.WriteLine("Finally, top try ended");
+        }
+        Console.WriteLine("Main ended");
+    }
+}
